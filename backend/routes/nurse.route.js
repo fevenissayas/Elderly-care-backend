a07cef54808db8ac12ce5c441362091a6cd2fe2d
Eyasu <eyasugetaneh0@gmail.com>
2025-05-02 20:38:29 +0300

    update routes

--- backend/routes/nurse.route.js
+++ backend/routes/nurse.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getUsers, login, signup } from "../controllers/nurse.controller.js"
+import { assignTask, getUsers, login, signup } from "../controllers/nurse.controller.js"
 
 const nurseRoute = express.Router()
 
 nurseRoute.post("/signup", signup)
 nurseRoute.post("/login", login)
+nurseRoute.post("/addTask", assignTask)
 nurseRoute.get("/", getUsers)
 
 export default nurseRoute
